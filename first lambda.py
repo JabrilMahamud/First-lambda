a5e1d3849067d938f0e26f1e52396c7be8fc7332
JabrilMahamud <jabril.mahamud@bp.com>
2022-03-23 17:16:37 +0000

    Create first lambda.py

---
+++ first lambda.py
@@ -0,0 +1,25 @@
+from ast import Expression
+import boto3
+dynamodb =boto3.resource("dynamodb",region_name="eu-west-1")
+table=dynamodb.Table("WH-0001-DYN_METADATA")
+
+
+tabledict=table.scan(
+    ProjectionExpression="#AN,account,#S",
+    ExpressionAttributeNames={
+        '#AN':'account-names',
+        '#S':'status',
+},
+
+)
+# git config --global user.email jabril.mahamud@bp.com
+# git config --global user.name JabrilMahamud
+tableList=list(tabledict.items())
+tableResponse=tableList[0][1]
+tableData=[]
+for i in range(len(tableResponse)):
+    tableData.append([tableResponse[i].get('account-name'),
+    tableResponse[i].get('account'),
+    tableResponse[i].get('status')])
+
+print (tableData)
