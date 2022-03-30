3deaa6fe49be68e12dedc3bb90869c79301a7a82
JabrilMahamud <jabril.mahamud@bp.com>
2022-03-30 10:08:16 +0100

    output data into the console now!

--- first lambda.py
+++ first lambda.py
@@ -1,14 +1,16 @@
-from ast import Expression
+# from ast import Expression
 import boto3
-import datetime
+# import datetime
 
-from __future__ import print_function
-def lambda_handle(event,context):
-    for record in event['records']:
-        print(record['account_name'])
-        print(record['account'])
-        print(record['status'])
-        print(datetime.datetime.now)
+#
+
+# from __future__ import print_function
+# def lambda_handle(event,context):
+#     for record in event['records']:
+#         print(record['account_name'])
+#         print(record['account'])
+#         print(record['status'])
+#         # print(datetime.datetime.now)
 
 dynamodb =boto3.resource("dynamodb",region_name="eu-west-1")
 table=dynamodb.Table("WH-0001-DYN_METADATA")
@@ -17,7 +19,7 @@ table=dynamodb.Table("WH-0001-DYN_METADATA")
 tabledict=table.scan(
     ProjectionExpression="#AN,account,#S",
     ExpressionAttributeNames={
-        '#AN':'account-names',
+        '#AN':'account-name',
         '#S':'status',
 },
 
