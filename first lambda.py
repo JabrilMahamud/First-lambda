5ef86c1ef026ff5e9d9f0537f85037793c30a3ef
JabrilMahamud <jabril.mahamud@bp.com>
2022-04-13 11:39:10 +0100

    Un-put everything in a function

--- first lambda.py
+++ first lambda.py
@@ -1,30 +1,15 @@
-# from ast import Expression
 from asyncore import write
 from time import time
-from unicodedata import name
 import boto3
 import csv
 import datetime as dt
 
-# from __future__ import print_function
-# def lambda_handle(event,context):
-#     for record in event['records']:
-#         print(record['account_name'])
-#         print(record['account'])
-#         print(record['status'])
-#         # print(datetime.datetime.now)
 def write2csv(tableList):
     data = [['Account-name'], ['Account-number'], ['status'],] 
     file = open('test.csv', 'w+', newline ='') 
     with file:     
         write = csv.writer(file) 
         write.writerows(tableData) 
-    # tableList=str(tableData)
-    # print(tableList)
-    # # f.write(tableList)
-    # csv.writer=csv.writer(f)
-    # writer.writerow(tableData)
-    # f.close(tableList)
 
 dynamodb =boto3.resource("dynamodb",region_name="eu-west-1")
 table=dynamodb.Table("WH-0001-DYN_METADATA")
@@ -44,9 +29,5 @@ for i in range(len(tableResponse)):
     tableData.append([tableResponse[i].get('account-name'),
     tableResponse[i].get('account'),
     tableResponse[i].get('status')])
-
-# def lambda_handler(event, context):
-#     for record in event['Records']:
-# get the changes here and save it
 
 write2csv(tableData)
