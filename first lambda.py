7c715db64a947e80b7439dba0a704631ffd8f4ed
JabrilMahamud <jabril.mahamud@bp.com>
2022-04-13 11:47:03 +0100

    merge issues resolved

--- first lambda.py
+++ first lambda.py
@@ -7,27 +7,27 @@ import boto3
 import csv
 from datetime import datetime
 
-    def write2csv(tableList):
-        data = [['Account-name'], ['Account-number'], ['status'],] 
-        file = open(datetime+'.csv', 'w+', newline ='') 
-        with file:     
-            write = csv.writer(file) 
-            write.writerows(tableData) 
+def write2csv(tableList):
+    data = [['Account-name'], ['Account-number'], ['status'],] 
+    file = open(datetime+'.csv', 'w+', newline ='') 
+    with file:     
+        write = csv.writer(file) 
+        write.writerows(tableData) 
 
-    tabledict=table.scan(
-        ProjectionExpression="#AN,account,#S",
-        ExpressionAttributeNames={
-            '#AN':'account-name',
-            '#S':'status',
-    },
-    )
+tabledict=table.scan(
+    ProjectionExpression="#AN,account,#S",
+    ExpressionAttributeNames={
+        '#AN':'account-name',
+        '#S':'status',
+},
+)
 
-    tableList=list(tabledict.items())
-    tableResponse=tableList[0][1]
-    tableData=[]
-    for i in range(len(tableResponse)):
-        tableData.append([tableResponse[i].get('account-name'),
-        tableResponse[i].get('account'),
-        tableResponse[i].get('status')])
+tableList=list(tabledict.items())
+tableResponse=tableList[0][1]
+tableData=[]
+for i in range(len(tableResponse)):
+    tableData.append([tableResponse[i].get('account-name'),
+    tableResponse[i].get('account'),
+    tableResponse[i].get('status')])
 
 write2csv(tableData)
