e1a249d6b9f798370fea1a8964dcdb669876fc2f
JabrilMahamud <jabril.mahamud@bp.com>
2022-04-29 06:28:10 +0100

    Minor syntax issue

--- first lambda.py
+++ first lambda.py
@@ -1,31 +1,46 @@
 from time import time
 import boto3
 import csv
-from datetime import datetime
+from datetime import date,datetime
 
 dynamoDB=boto3.resource("dynamodb",region_name='eu-west-1')
 table=dynamoDB.Table('WS-Z038_Metadata')
 
-def write2csv(tableList):
+def lambda_handler(event,context):
+    csv_name = 'WS-Z038-'+str(date.today())+'-'+datetime.now().strftime("%H-%M")+'.csv'
+    tableData=[]
     data = [['Account-name'], ['Account-number'], ['status'],] 
-    file = open(datetime+'.csv', 'w+', newline ='') 
-    with file:     
-        write = csv.writer(file) 
-        write.writerows(tableData) 
-tabledict=table.scan(
-    ProjectionExpression="#AN,account,#S",
-    ExpressionAttributeNames={
-        '#AN':'account-name',
-        '#S':'status',
+    file = open(csv_name, 'w+', newline ='') 
+    tabledict=table.scan(
+        ProjectionExpression="#AN,account,#S",
+        ExpressionAttributeNames={
+            '#AN':'account-name',
+            '#S':'status',
 },
 )
 
-tableList=list(tabledict.items())
-tableResponse=tableList[0][1]
-tableData=[]
-for i in range(len(tableResponse)):
-    tableData.append([tableResponse[i].get('account-name'),
-    tableResponse[i].get('account'),
-    tableResponse[i].get('status')])
-
-write2csv(tableData)
+    tableList=list(tabledict.items())
+    tableResponse=tableList[0][1]
+    for index in range(len(tableResponse)):
+        tableData.append([tableResponse[index].get('account-name'),
+        tableResponse[index].get('account'),
+        tableResponse[index].get('status')])
+    
+    S3_Client = boto3.client("s3", region_name='eu-west-1')
+    allBuckets = S3_Client.list_buckets().get('Buckets')
+    foundBucket = False
+    j = 0
+    while foundBucket == False and j < len(allBuckets) :
+        if allBuckets[j].get('Name') == 'ws-z038-metadata-bucket':
+            foundBucket = True
+        j+=1
+    if foundBucket == False:
+        S3_Client.create_bucket(
+            Bucket='ws-z038-metadata-bucket',
+            CreateBucketConfiguration= {
+                'LocationConstraint': 'eu-west-1'
+            }
+            
+        )
+    
+    S3_Client.upload_file('/tmp/'+csv_name,'ws-z038-metadata-bucket',csv_name)
